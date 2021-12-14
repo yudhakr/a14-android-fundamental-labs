303868821cbe4757b745953a6897a2dc9300bf7f
yudhakr <84507343+yudhakr@users.noreply.github.com>
2021-12-14 13:40:00 +0700

    lihat perubahan

--- fundamental/LatihanRecyclerView/kotlin/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
+++ fundamental/LatihanRecyclerView/kotlin/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -1,4 +1,4 @@
-package com.dicoding.picodiploma.myrecyclerview.adapter
+package com.dicoding.myrecyclerview
 
 import android.view.LayoutInflater
 import android.view.View
@@ -6,46 +6,37 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.dicoding.picodiploma.myrecyclerview.R
-import com.dicoding.picodiploma.myrecyclerview.model.Hero
-import java.util.*
 
-/**
- * Created by sidiqpermana on 10/28/16.
- */
+class ListHeroAdapter (private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
-class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
-    private lateinit var onItemClickCallback: OnItemClickCallback
 
-    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
-        this.onItemClickCallback = onItemClickCallback
-    }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
-        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_hero, viewGroup, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
         return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
         val (name, description, photo) = listHero[position]
         holder.imgPhoto.setImageResource(photo)
         holder.tvName.text = name
         holder.tvDescription.text = description
-        holder.itemView.setOnClickListener {
-            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
-        }
-
     }
 
-    override fun getItemCount(): Int = listHero.size
+    override fun getItemCount(): Int  = listHero.size{
+        TODO("Not yet implemented")
 
+    }
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
-
     interface OnItemClickCallback {
         fun onItemClicked(data: Hero)
     }
+
 }
+
+
